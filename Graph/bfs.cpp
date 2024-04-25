dcff3198ce4c4f5da0abe466b407093d4d844ec0
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-04-25 12:20:47 +0530

    bfs

---
+++ Graph/bfs.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX / 2;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+    }
+    return 0;
+}
