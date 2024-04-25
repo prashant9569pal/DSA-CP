cf8aeccf371c6f903406b950a257b475b8c668bd
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-04-25 12:31:44 +0530

    closeness

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -1,21 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-
-const int MOD = 1e9 + 7;
-const int INF = LLONG_MAX / 2;
-
-signed main()
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int tc;
-    cin >> tc;
-    while (tc--)
+    int test_cases;
+    cin >> test_cases;
+    for (int t = 1; t <= test_cases; t++)
     {
+        int n;
+        long long k;
+        cin >> n >> k;
+        vector<long long> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr.begin(), arr.end());
+
+        long long max_val = arr[n - 1];
+        for (int i = 0; i < n - 1; i++)
+        {
+            long long tmp = (max_val - arr[i]) / k;
+            arr[i] = arr[i] + (tmp * k);
+        }
+        long long min_val = arr[n - 1];
+        for (int i = 0; i < n; i++)
+        {
+            min_val = min(min_val, arr[i]);
+        }
+        long long ans1 = max_val - min_val;
+        sort(arr.begin(), arr.end());
+        max_val = arr[n - 1];
+        for (int i = 0; i < n; i++)
+        {
+            ans1 = min(ans1, max_val - arr[i]);
+            max_val = max(max_val, arr[i] + k);
+        }
+        cout << ans1 << endl;
     }
     return 0;
 }
