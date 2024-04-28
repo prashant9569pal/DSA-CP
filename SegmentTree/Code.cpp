d700d0e9908380ea69de2fbb13f12c832c852e6a
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-04-28 16:11:27 +0530

    rightMostSetBit

--- SegmentTree/Code.cpp
+++ SegmentTree/Code.cpp
@@ -1,90 +1,37 @@
-// source code
-// new code  please push it here
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long int
 
-struct segmentTree
+#define endl '\n'
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX / 2;
+
+signed main()
 {
-    int n;
-    vector<int> st;
-    void init(int _n)
-    {
-        this->n = _n;
-        st.resize(4 * n);
-    }
-    void build(int start, int end, int node, vector<int> &v)
-    {
-        // leaf node
-        if (start == end)
-        {
-            st[node] = v[start];
-            return;
-        }
-        int mid = (start + end) / 2;
-        // left subtree is (start, mid)
-        build(start, mid, 2 * node + 1, v);
-        // right subtree is (mid+1, end)
-        build(mid + 1, end, 2 * node + 2, v); // corrected the index for right child
-        st[node] = st[node * 2 + 1] + st[node * 2 + 2];
-    }
-    void build(vector<int> &v)
-    {
-        build(0, v.size() - 1, 0, v);
-    }
-    int query(int start, int end, int l, int r, int node)
-    {
-        // non overlapping case
-        if (start > r || end < l)
-            return 0;
-        // complete overlap case
-        if (start >= l && end <= r)
-            return st[node];
-        // partial case
-        int mid = (start + end) / 2;
-        int q1 = query(start, mid, l, r, 2 * node + 1);
-        int q2 = query(mid + 1, end, l, r, 2 * node + 2);
-        return q1 + q2;
-    }
-    int query(int l, int r)
-    {
-        return query(0, n - 1, l, r, 0);
-    }
-    void update(int start, int end, int node, int index, int value)
-    {
-        // basee case
-        if (start == end)
-        {
-            st[node] = value;
-            return;
-        }
-        int mid = (start + end) / 2;
-        if (index <= mid)
-        {
-            update(start, mid, 2 * node + 1, index, value);
-        }
-        else
-        {
-            update(mid + 1, end, 2 * node + 2, index, value);
-        }
-        st[node] = st[node * 2 + 1] + st[node * 2 + 2];
-        return;
-    }
-    void update(int x, int y)
-    {
-        update(0, n - 1, 0, x, y);
-    }
-};
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int32_t main()
-{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
 
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    segmentTree tree;
-    tree.init(v.size());
-    tree.build(v);
-    cout << tree.query(0, 6) << endl;
-    tree.update(2, 458);
-    cout << tree.query(0, 5) << endl;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int ans = 0;
+        bool first = true;
+        for (int i = 0; i < n; i++)
+        {
+            ans = max(arr[i] + ans, arr[i] * ans) % MOD;
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
